76432a4fce43e9736bd15a1361b3f000754ec644
Marcin Swornowski <swor71@gmail.com>
2018-09-26 22:38:55 +0200

    first app from vue-cli

---
+++ Applications/starbase/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app" class="container-fluid">
+    <h3>{{title}}</h3>
+    <div class="row">
+      <Character 
+        v-for="(id, index) in initial_ids"
+        :id="id"
+        :key="index"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Character from './components/Character.vue';
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      title: 'Generate the team',
+      initial_ids: [1, 13, 14]
+    };
+  },
+  components: {
+    Character
+  }
+};
+</script>
+
+<style>
+</style>
